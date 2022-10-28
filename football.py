bbed37c6a8d9ee0cb51108d2f38bdee6ba189e77
AngelFireLA <55795775+AngelFireLA@users.noreply.github.com>
2022-10-28 23:06:17 +0200

    Add files via upload

--- football.py
+++ football.py
@@ -2,11 +2,91 @@ from random import *
 import csv
 import os
 import pandas as pd
-players = pd.read_csv('football.csv', header=None, dtype={0: str}).dropna().set_index(0).squeeze().to_dict()
-#sortedplayers = sorted(players[11].items(), key=lambda x:x[1])
-sortedplayers = sorted(players[11].items(), key=lambda kv: (kv[1], kv[0]))
-for player in range(len(sortedplayers)):
-	#print(player, players[11][player])
-	print(sortedplayers)
+
+season = 1
+
+players = {}
+# sortedplayers = sorted(players[1].items(), key=lambda kv: (kv[1], kv[0]))
+# print(sortedplayers)
+
+
+
+def initialize_database():
+	global players
+	players = pd.read_csv('football.csv', header=None, dtype={0: str}).dropna().set_index(0).squeeze().to_dict()
+	# for player in players[season]:
+	# 	print(player, players[season][player])
+
+def pair_players():
+	player_list = []
+	for player in players[season]:
+		player_list.append(player)
+	player_list_lenght = len(player_list)+1
+	#print(player_list)
+	for pairs in range(int(player_list_lenght/2)):
+		player1 = player_list[randint(0, len(player_list)-1)]
+		player_list.remove(player1)
+		player2 = player_list[randint(0, len(player_list)-1)]
+		player_list.remove(player2)
+		print(player1, "vs", player2)
+		if randint(0, 10) >= randint(0, 10):
+			player_list.append(player1)
+		else:
+			player_list.append(player2)
 	print()
-	
+	for pairs in range(int(player_list_lenght / 4)):
+		player1 = player_list[randint(0, len(player_list) - 1)]
+		player_list.remove(player1)
+		player2 = player_list[randint(0, len(player_list) - 1)]
+		player_list.remove(player2)
+		print(player1, "vs", player2)
+		if randint(0, 10) >= randint(0, 10):
+			player_list.append(player1)
+		else:
+			player_list.append(player2)
+	print()
+	for pairs in range(int(player_list_lenght / 8)):
+		player1 = player_list[randint(0, len(player_list) - 1)]
+		player_list.remove(player1)
+		player2 = player_list[randint(0, len(player_list) - 1)]
+		player_list.remove(player2)
+		print(player1, "vs", player2)
+		if randint(0, 10) >= randint(0, 10):
+			player_list.append(player1)
+		else:
+			player_list.append(player2)
+	print()
+	for pairs in range(int(player_list_lenght / 16)):
+		player1 = player_list[randint(0, len(player_list) - 1)]
+		player_list.remove(player1)
+		player2 = player_list[randint(0, len(player_list) - 1)]
+		player_list.remove(player2)
+		print(player1, "vs", player2)
+		if randint(0, 10) >= randint(0, 10):
+			player_list.append(player1)
+		else:
+			player_list.append(player2)
+	print()
+	for pairs in range(int(player_list_lenght / 32)):
+		player1 = player_list[randint(0, len(player_list) - 1)]
+		player_list.remove(player1)
+		player2 = player_list[randint(0, len(player_list) - 1)]
+		player_list.remove(player2)
+		print(player1, "vs", player2)
+		if randint(0, 10) >= randint(0, 10):
+			player_list.append(player1)
+		else:
+			player_list.append(player2)
+	print(player_list)
+
+def main():
+	initialize_database()
+	pair_players()
+
+
+def getPlayerRating(season : int, name) -> int:
+	return int(players[season][name])
+
+main()
+
+
